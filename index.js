90c5ba6cb59d0c53cadd47d48cefe21ffee7dc23
rakib38324 <rakib3832@gmail.com>
2022-11-08 17:30:11 +0600

    Commit the get with limit service

--- index.js
+++ index.js
@@ -29,10 +29,26 @@ async function run(){
       
 
         app.get('/services',async (req,res)=>{
-            const query = {};
-            const cursor = serviceCollection.find(query)
-            const services = await cursor.toArray();
+            const query = req.query.limit || 0;
+            const cursor = serviceCollection.find({}).sort({_id:-1})
+            const services = await cursor.limit(parseInt(query)).toArray();
             res.send(services)
+        })
+
+        app.post('/services',async (req,res)=>{
+            const service = req.body;
+            console.log(service);
+            const result = await serviceCollection.insertOne(service);
+            res.send(result);
+        })
+
+        app.get('/services/:id',async(req,res)=>{
+            const id = req.params.id;
+
+            const query = {_id: ObjectId(id)};
+
+            const service = await serviceCollection.findOne(query)
+            res.send(service);
         })
        
 
