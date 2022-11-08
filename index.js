cc19737bac806a7e68c7f03343ea5e1dc13b92e8
rakib38324 <rakib3832@gmail.com>
2022-11-08 14:16:17 +0600

    Get all data from server

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
-
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,13 +15,35 @@ app.use(express.json());
 const USER = process.env.DB_USER
 const PASSWORD = process.env.DB_PASSWORD
 
-
+console.log(USER, PASSWORD)
 
 const uri = `mongodb+srv://${USER}:${PASSWORD}@cluster0.qlqg855.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+async function run(){
+
+    try{
+        const serviceCollection = client.db('rtr-cloud-kitchen').collection('services');
+      
+
+        app.get('/services',async (req,res)=>{
+            const query = {};
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+       
+
+    }
+    finally{
+
+    }
+
+}
+
+run().catch(error => console.log(error))
 
 
 
@@ -31,5 +53,5 @@ app.get('/',(req,res)=>{
 });
 
 app.listen(port, ()=>{
-    console.log(`Genius car server running on ${port}`);
+    console.log(`RTR Cloud Kitchen Server running on ${port}`);
 })
