3eb469c437bf3dc43de2d5ef22afca25c03c6b86
rakib38324 <rakib3832@gmail.com>
2022-11-09 17:45:43 +0600

    Commit the delete sectoin

--- index.js
+++ index.js
@@ -89,6 +89,13 @@ async function run(){
             res.send(review);
         })
 
+        app.delete('/review/:id',async(req,res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await reviewCollection.deleteOne(query);
+            res.send(result);
+        })
+
 
     }
     finally{
